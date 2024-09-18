d53c9fa3b4a630386825df7386541469b6c35301
PedroPsousilva <pedro.soupsilva@gmail.com>
2024-09-18 12:55:04 -0300

    correcaoparprimo

--- correcaoParPrimoPedro/src/controllers/controllerNumbers.js
+++ correcaoParPrimoPedro/src/controllers/controllerNumbers.js
@@ -1,6 +1,10 @@
 module.exports = class controllerNumbers{
     static async checkPar(req,res){
         const {numero} = req.body;
+        
+        if(typeof numero !== "number"){
+            return res.status(400).json({message:"Não é um numero!!!"})
+        }
 
         if(numero % 2 == 0){
             return res.status(200).json({message:"Numero Par!"});
